5d28d239c3a7278cd0fe97893e4d0ca5449cf839
Oleksandr Dykun <sashaluckyman@gmail.com>
2018-11-16 15:47:21 -0800

    completed  seting up redux

---
+++ src/reducers/index.js
@@ -0,0 +1,9 @@
+import { combineReducers } from 'redux';
+import listReducer from './list_reducer';
+
+
+const rootReducer = combineReducers({
+    list: listReducer,
+});
+
+export default rootReducer;
